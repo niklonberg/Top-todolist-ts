7031b9006f88092f8ca9e3e9d8ca36706a6043d3
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-20 12:15:17 +0100

    add types to elements created by createBaseGroupHTML

--- src/components/modules/utils/createBaseGroupHTML.ts
+++ src/components/modules/utils/createBaseGroupHTML.ts
@@ -1,11 +1,19 @@
 import createElement from './createElement';
 
-function createBaseGroupHTML(listGroupSelection) {
-  const h1 = createElement('h1', 'test', 'grouping-title');
+function createBaseGroupHTML(listGroupSelection: HTMLLIElement) {
+  const h1: HTMLHeadingElement = createElement(
+    'h1',
+    'test',
+    'grouping-title',
+  ); /* as HTMLHeadingElement */
   h1.textContent =
     listGroupSelection?.querySelector('h3').textContent ?? 'All Tasks';
 
-  const list = createElement('ul', 'test2', 'curr-grouping-todos');
+  const list: HTMLUListElement = createElement(
+    'ul',
+    'test2',
+    'curr-grouping-todos',
+  ); /* as HTMLUListElement */
 
   return [h1, list];
 }

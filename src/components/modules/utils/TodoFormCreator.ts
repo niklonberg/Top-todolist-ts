022166e654eb59c5e7b51c32e60720393460d7b0
Nikolai Lønberg <niklonberg@gmail.com>
2024-01-07 13:50:33 +0100

    add styling for todo form

--- src/components/modules/utils/TodoFormCreator.ts
+++ src/components/modules/utils/TodoFormCreator.ts
@@ -3,16 +3,42 @@ import createElement from './createElement';
 function TodoFormFactory() {
   const form = createElement<HTMLFormElement>('form', 'todo-form');
   const formTemplate = `
-    <label for="title">Title: </label>
-    <input type="text" name="title" id="title" required/>
-    <label for="description">Description: </label>
-    <input type="text" name="description" id="description"/>
+  <div class="input-container">
+    <label for="title" class="text-input-label">Title</label>
+    <input
+      class="text-input"
+      type="text"
+      name="title"
+      id="title"
+      required
+    />
+  </div>
+  <div class="input-container">
+    <label for="description" class="text-input-label"
+      >Description
+    </label>
+    <input
+      type="text"
+      name="description"
+      id="description"
+      class="text-input"
+    />
+  </div>
+  <div class="input-container">
     <label for="isUrgent">Urgent</label>
-    <input type="checkbox" name="isUrgent" id="isUrgent" value="true"/>
-    <label for="dueDate">Have a date its due for ?</label>
-    <input type="date" name="dueDate" id="dueDate">
-    <button type="submit">Confirm</button>
-    <button type="button" class="cancel-form">Cancel</button>
+    <input
+      type="checkbox"
+      name="isUrgent"
+      id="isUrgent"
+      value="true"
+    />
+  </div>
+  <div class="input-container">
+    <label for="dueDate">Duedate</label>
+    <input type="date" name="dueDate" id="dueDate" />
+  </div>
+  <button type="submit">Confirm</button>
+  <button type="button" class="cancel-form">Cancel</button>
   `;
   form.innerHTML = formTemplate;
   return form;

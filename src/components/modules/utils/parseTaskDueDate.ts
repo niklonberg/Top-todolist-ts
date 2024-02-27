2305dbc08e67195bf8939c6486525affb3d35c33
Nikolai Lønberg <niklonberg@gmail.com>
2024-02-27 13:18:09 +0100

    edit parseTaskDuedate to also convert subtasks.dueDate to javascript object

--- src/components/modules/utils/parseTaskDueDate.ts
+++ src/components/modules/utils/parseTaskDueDate.ts
@@ -1,9 +1,14 @@
 import { Task } from './interfaces';
 
 function parseTaskDueDate(task: Task): Task {
+  const parsedSubtasksDuedate = task.subtasks.map((subtask) => ({
+    ...subtask,
+    dueDate: subtask.dueDate ? new Date(subtask.dueDate) : null,
+  }));
   return {
     ...task,
     dueDate: new Date(task.dueDate),
+    subtasks: parsedSubtasksDuedate,
   };
 }
 

7297a0340fa977ee2262e36cafa2f87c88e29159
Nikolai Lønberg <niklonberg@gmail.com>
2024-03-08 12:59:08 +0100

    install sass, sass-loader, config webpack-config.js

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },

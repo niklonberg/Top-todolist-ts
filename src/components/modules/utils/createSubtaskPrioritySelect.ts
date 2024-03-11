eab6183d99ae1a96d53926fe02244d122215615d
Nikolai Lønberg <niklonberg@gmail.com>
2024-03-11 13:35:16 +0100

    create addOnPriorityChangeFunctionality

--- src/components/modules/utils/createSubtaskPrioritySelect.ts
+++ src/components/modules/utils/createSubtaskPrioritySelect.ts
@@ -1,29 +1,35 @@
 import createElement from './createElement';
 import { type PriorityLevel } from '../definitions/types';
 
-function createSubtaskPrioritySelect(prioritySortOrder: PriorityLevel) {
+function addOnPriorityChangeFunctionality(element: HTMLSelectElement) {
+  element.addEventListener('change', (event) => {
+    const selectedPriority = (event.target as HTMLSelectElement)
+      .value as PriorityLevel;
+    console.log(selectedPriority);
+  });
+}
+
+function createSubtaskPrioritySelect(
+  prioritySortOrder: PriorityLevel = 'High',
+) {
   const prioritySelectContainer = createElement('div');
   const template = `
-  <label for="prioritySortOrder">Priority:</label>
+  <label for="prioritySortOrder">Priority sort order:</label>
   <select id="prioritySortOrder" class="priority-select" name="prioritySortOrder">
+    <option value="High" ${
+      prioritySortOrder === 'High' ? 'selected' : ''
+    }>High</option>  
     <option value="Low" ${
       prioritySortOrder === 'Low' ? 'selected' : ''
     } >Low</option>
-    <option value="High" ${
-      prioritySortOrder === 'High' ? 'selected' : ''
-    }>High</option>
   </select>`;
   prioritySelectContainer.innerHTML = template;
 
-  const selectElement =
+  addOnPriorityChangeFunctionality(
     prioritySelectContainer.querySelector<HTMLSelectElement>(
       '#prioritySortOrder',
-    );
-  selectElement.addEventListener('change', (event) => {
-    const selectedPriority = (event.target as HTMLSelectElement)
-      .value as PriorityLevel;
-    console.log('hi');
-  });
+    ),
+  );
 
   return prioritySelectContainer;
 }

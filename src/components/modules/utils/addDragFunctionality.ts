eea43e81ead6c27f5db82193fa0fbb0f07d6bbce
Nikolai Lønberg <niklonberg@gmail.com>
2024-03-12 13:53:59 +0100

    reimplement base reorder functionality

--- src/components/modules/utils/addDragFunctionality.ts
+++ src/components/modules/utils/addDragFunctionality.ts
@@ -26,42 +26,42 @@ function getDragAfterElement(container: HTMLElement, y: number) {
 
 function addDragFunctionality(
   ul: HTMLUListElement,
-  dataManager?: TaskManagerInterface,
+  TaskManager?: TaskManagerInterface,
 ) {
   const listItems = [...ul.children];
   listItems.forEach((li) => {
     li.setAttribute('draggable', 'true');
     li.classList.add('draggable');
   });
-  // ul.addEventListener('dragstart', (e) => {
-  //   const target = e.target as HTMLElement;
-  //   if (target.classList.contains('draggable'))
-  //     target.classList.add('dragging');
-  //   ul.classList.add('highlight-drag-area');
-  // });
-  // ul.addEventListener('dragend', (e) => {
-  //   const target = e.target as HTMLElement;
-  //   if (target.classList.contains('draggable'))
-  //     target.classList.remove('dragging');
-  //   const todoItems = [...ul.querySelectorAll('.list-item')];
-  //   console.log('target: ', target);
-  //   console.log(todoItems.indexOf(target));
-  //   dataManager?.reorderTodo(
-  //     todoItems.indexOf(target),
-  //     target as TaskListItem,
-  //   );
-  //   ul.classList.remove('highlight-drag-area');
-  // });
-  // ul.addEventListener('dragover', (e) => {
-  //   e.preventDefault();
-  //   const afterElement = getDragAfterElement(ul, e.clientY);
-  //   const dragElement = ul.querySelector('.dragging');
-  //   if (afterElement == null) {
-  //     ul.appendChild(dragElement);
-  //   } else {
-  //     ul.insertBefore(dragElement, afterElement);
-  //   }
-  // });
+  ul.addEventListener('dragstart', (e) => {
+    const target = e.target as HTMLElement;
+    if (target.classList.contains('draggable'))
+      target.classList.add('dragging');
+    ul.classList.add('highlight-drag-area');
+  });
+  ul.addEventListener('dragend', (e) => {
+    const target = e.target as HTMLElement;
+    if (target.classList.contains('draggable'))
+      target.classList.remove('dragging');
+    const todoItems = [...ul.querySelectorAll('.list-item')];
+    console.log('target: ', target);
+    console.log(todoItems.indexOf(target));
+    // TaskManager?.reorderTodo(
+    //   todoItems.indexOf(target),
+    //   target as TaskListItem,
+    // );
+    ul.classList.remove('highlight-drag-area');
+  });
+  ul.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(ul, e.clientY);
+    const dragElement = ul.querySelector('.dragging');
+    if (afterElement == null) {
+      ul.appendChild(dragElement);
+    } else {
+      ul.insertBefore(dragElement, afterElement);
+    }
+  });
 }
 
 export default addDragFunctionality;
